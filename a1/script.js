b7fc2859ed2f94f62d3f15594c043af7c1b92303
fernandezpaulo0724 <fernandezpaulo0724@gmail.com>
2021-10-07 20:29:04 +0800

    added activity code

---
+++ a1/script.js
@@ -0,0 +1,51 @@
+const getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let posts ='';
+        data.forEach((post) =>{ 
+            posts += `
+                <div class="border">
+                <img src="blank-profile.png" width="150" height="200" class="center">               
+                <h5>Name: ${post.name}</h5>
+                <p>Email: ${post.email}</p>
+                <p>Company: ${post.company.name}</p>
+                <p>Phone: ${post.phone}</p>
+                </div>
+            `
+        })
+        document.querySelector('#output').innerHTML = posts;
+    })
+    .catch((err) => {
+        console.log('rejected')
+    })
+}
+
+const addPost = (e) => {
+    e.preventDefault();
+    let title = document.querySelector("#title").value;
+    let body = document.querySelector("#body").value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        headers: {
+            "Accept": "application/json, text/plain",
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({title: title, body: body}) 
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+}
+
+const btn1 = document.querySelector('#btn1')
+btn1.addEventListener('click', getData)
+
+const addForm = document.querySelector('#addPost')
+addForm.addEventListener('submit', addPost);
